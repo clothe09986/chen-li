39a90e435f3c50b26095af8563a5936bc16acd5b
logan <clothe09986@gmail.com>
2022-06-13 07:45:26 +0800

    [FEATURE] adjust env variables

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ require('dotenv').config()
 const env = {
   HASURA_API: process.env.HASURA_API,
   HASURA_API_SECRET: process.env.HASURA_API_SECRET,
-  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
-  AWS_ACCESS_KEY_SECRET: process.env.AWS_ACCESS_KEY_SECRET,
+  AWS_ACCESS_KEY: process.env.ACCESS_KEY,
+  AWS_ACCESS_KEY_SECRET: process.env.ACCESS_KEY_SECRET,
 }
 
 const nextConfig = {
